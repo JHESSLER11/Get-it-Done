6ca8d75876982b345f16745d9aeb49eb25150547
JHessler <jhessler11@gmail.com>
2022-01-31 21:49:08 -0500

    added web API

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -1,5 +1,10 @@
 var getUsersRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response)
+    {
+        response.json().then(function(data) {
+    console.log(response);
+        });
+    });
 };
-
+console.log("outside")
 getUsersRepos()
