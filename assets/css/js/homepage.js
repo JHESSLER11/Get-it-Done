c6abb3d6c9457491a22c1c2ee7bd3ab9fb778e1c
JHessler <jhessler11@gmail.com>
2022-02-01 18:53:46 -0500

    6.2.5

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -1,3 +1,31 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("repo-search-term");
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //get value from input element 
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUsersRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert('Please enter a Github username')
+    }
+    console.log(event);
+}
+
+var displayRepos = function(repos, searchTerm) {
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
 var getUsersRepos = function(user) {
     // format the github api url
     var apiUrl = "http://api.github.com/users/" +user + "/repos";
@@ -5,7 +33,7 @@ var getUsersRepos = function(user) {
     //make a request to the url 
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
